dae96e9cd21f0e8de1d48ffc0be9338c4c781f4a
unknown <bsp.vika@gmail.com>
2023-07-07 14:35:13 +0200

    Update steps timeout

--- src/test/steps/personalLiabilityCalculatorSteps.ts
+++ src/test/steps/personalLiabilityCalculatorSteps.ts
@@ -2,7 +2,7 @@ import { Given, When, Then, setDefaultTimeout } from "@cucumber/cucumber"
 import { expect, defineConfig, Locator } from "@playwright/test"
 import { pageFixture } from "../hooks/pageFixture";
 
-setDefaultTimeout(60 * 1000 * 2);
+setDefaultTimeout(50 * 1000 * 2);
 
 
 Given('that I can open www.verivox.de', async function () {
