dd30838ed2cb6f3c04e6a7c95ac601ca00033ce8
Viktoriia Bespiatova <bsp.vika@gmail.com>
2023-07-08 16:16:11 +0200

    Update BrowserManager.ts

--- src/helper/browsers/BrowserManager.ts
+++ src/helper/browsers/BrowserManager.ts
@@ -1,7 +1,7 @@
 import { chromium, firefox, LaunchOptions, webkit } from "@playwright/test";
 
 const options: LaunchOptions = {
-    headless: false,
+    headless: true,
 }
 export const invokeBrowser = () => {
     const browserType = process.env.BROWSER;
